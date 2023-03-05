25a1a0ec4da797af4d0c3011e066ec8076f19acf
Aneesh <aneeshveeramachaneni@gmail.com>
2023-03-04 16:24:53 -0800

    "About" page prototype

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Home from './pages/Home';
 import Framework from './pages/Framework';
+import Research from './pages/Research';
+import About from './pages/About';
 import {
   BrowserRouter as Router,
   Route,
   Routes,
 } from "react-router-dom";
-import Research from './pages/Research';
 
 function App() {
   return (
@@ -19,6 +20,8 @@ function App() {
           <Route exact path="/framework" element={<Framework />}>
           </Route>
           <Route exact path="/research" element={<Research />}>
+          </Route>
+          <Route exact path="/about" element={<About />}>
           </Route>
        </Routes>
       </Router>
