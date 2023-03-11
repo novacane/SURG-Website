ac735e16cfa987c61f2f30801ef8b8b20e86aacb
Aneesh <aneeshveeramachaneni@gmail.com>
2023-03-10 23:28:26 -0800

    added links to footer, fixed research image to not resize weirdly

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,21 @@ import "./Footer.css"
 
 const Footer = () => {
     return (
+        
         <div className = 'FooterWrap'>
             <div className = 'FooterContainer'>
                 <div className = 'footer-item'>Berkeley, CA</div>
             </div>
             <div className = 'FooterContainer'>
                 {/* Change to a tag and add email later */}
-                <div className = 'footer-item'>Email</div>
+                <div className = 'footer-item'>
+                    <a href="mailto:xsustainabilityreview.berkeley@gmail.com  ">Email</a>
+                </div>
             </div>
             <div className = 'FooterContainer'>
-                <div className = 'footer-item'>SURG</div>
+                <div className = 'footer-item'>
+                    <a href="https://surg.berkeley.edu/"  target="_blank" rel="noreferrer">SURG</a>
+                </div>
             </div>
         </div>
     )
